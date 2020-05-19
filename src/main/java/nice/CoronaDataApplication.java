8675b5334b38daeb1c5b70eb6261feaa719bbf19
pnegi-centrica <preeti.negi@centrica.com>
2020-05-19 16:49:33 +0100

    first commit

---
+++ src/main/java/nice/CoronaDataApplication.java
@@ -0,0 +1,13 @@
+package nice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nice/CoronaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package nice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
